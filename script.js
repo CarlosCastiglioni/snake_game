f64b0b68c1881763d33c8ad20b4068607cc3a2dd
Carlos Alberto Castiglioni Junior <juniorcastiglioni@gmail.com>
2021-02-04 11:31:07 -0300

    creating snake

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 let canvas = document.getElementById("snake")
 let context = canvas.getContext("2d")
+
 let box = 32
 
- function criarBG() {
-   context.fillStyle = "purple"
-   context.fillRect(0, 0, 16 * box, 16 * box)
- }
+let snake = []
+snake[0] = {
+  x: 8 * box,
+  y: 8 * box
+}
 
- criarBG()
+function createBG() {
+  context.fillStyle = "purple"
+  context.fillRect(0, 0, 16 * box, 16 * box)
+}
+
+function createSnake() {
+  for (i = 0; i < snake.length; i++) {
+    context.fillStyle = "yellow"
+    context.fillRect(snake[i].x, snake[i].y, box, box)
+  }
+}
+
+createBG()
+createSnake()
